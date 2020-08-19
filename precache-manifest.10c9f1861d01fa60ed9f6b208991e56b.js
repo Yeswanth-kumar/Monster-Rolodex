c9db63116a27e1ccd1640a92038efd78f1d6622f
Yeswanth-Kumar <yeswanthnova@gmail.com>
2020-08-19 18:54:40 +0530

    Updates

---
+++ precache-manifest.10c9f1861d01fa60ed9f6b208991e56b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6121bf7e21ee3a2ea62838fa1ef147b",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "2009e2e7553bed003148",
+    "url": "/Monster-Rolodex/static/css/main.079b0f00.chunk.css"
+  },
+  {
+    "revision": "047cf2e9d1b897580e7f",
+    "url": "/Monster-Rolodex/static/js/2.9a71f64d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.9a71f64d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2009e2e7553bed003148",
+    "url": "/Monster-Rolodex/static/js/main.16075676.chunk.js"
+  },
+  {
+    "revision": "5afc474d2d9fccc33f5d",
+    "url": "/Monster-Rolodex/static/js/runtime-main.f215c452.js"
+  }
+]);
